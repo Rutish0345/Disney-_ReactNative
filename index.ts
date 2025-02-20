5ce8b1ce79c7c1ce086d7d61a93a5d7d4426cb24
Rutish0345 <ruthbarrientosangeles@gmail.com>
2025-02-19 23:54:32 -0600

    Add files via upload

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import Disney from './app/screens/Disney';
+import Calculadora from './app/screens/Calculadora'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Disney);
+registerRootComponent(Calculadora);
